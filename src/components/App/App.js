2bcf1f4c0b202084343c8b16b24f55e0bcd2e984
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-02-03 19:26:18 +0300

    added reorder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,10 +23,41 @@ const initialState = {
 function App() {
   const [state, setState] = useState(initialState)
 
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result
+
+    if (!destination) return
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) return
+
+    const column = state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+
+    setState(newState)
+  }
+
   return (
     <div className="app">
       <DragDropContext
-        
+        onDragEnd={onDragEnd}
       >
         {state.columnOrder.map((columnId) => {
           const column = state.columns[columnId]
